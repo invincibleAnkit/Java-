8d074236f97d78970154c52dc77520b8e76bf34e
invincibleAnkit <rkrockcena461@gmail.com>
2025-12-24 19:37:25 +0000

    commit 4

--- test.java
+++ test.java
@@ -1,17 +1,28 @@
+import java.util.Scanner;
 
- import java.util.Scanner;
-public class test { 
-    public static void main(String[] args){
+public class test {
+    public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
-        for(int i=2;i<=n-1;i++){
-            if(n%i==0){
-            System.out.println(" True");
-            break;
-            }else{
-            System.out.println("False");
+
+        boolean isComposite = false;
+
+        for (int i = 2; i <= n - 1; i++) {
+
+            if (n % i == 0) {     
+                isComposite = true;
+                break;          
+            }
         }
+        if (isComposite) {
+            System.out.println("True");  
+        } else {
+            System.out.println("False");  
         }
-    } 
-    
+
+        sc.close();
+    }
+}
+
 
