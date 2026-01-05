52fc5e6b28fd9afa34d40f11bea925ad1f2782ba
invincibleAnkit <rkrockcena461@gmail.com>
2026-01-06 00:12:11 +0530

    Correct nested loop for number square pattern
    Fixed nested loop structure for printing numbers.

--- patternprinting/numbersquare.java
+++ patternprinting/numbersquare.java
@@ -4,12 +4,18 @@ public class numbersquare {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter n :");
         int n = sc.nextInt();
-        for (int i=1;i<=n;i++){
+         for (int i=1;i<=n;i++){
           for (int j=1;j<=n;j++){
             System.out.print(j+"");
            }
           System.out.println();
         }
+        // for (int i=1;i<=n;i++){
+        //   for (int j=1;j<=n;j++){
+        //     System.out.print((char)(j+64)+"");
+        //    }
+        //   System.out.println();
+        // }
         sc.close();
       }
     }
