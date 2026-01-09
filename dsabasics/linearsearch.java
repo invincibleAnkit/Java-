816c95b3a6a8841fa30b26ef9db78fd94a0d46b8
invincibleAnkit <rkrockcena461@gmail.com>
2026-01-09 18:49:38 +0000

    commit

--- dsabasics/linearsearch.java
+++ dsabasics/linearsearch.java
@@ -8,17 +8,24 @@ public class linearsearch {
          int x = sc.nextInt();
          System.out.print("enter Array size:");
          int n = sc.nextInt();
+         int i;
     int[] arr = new int[n];
-   for (int i = 0; i<n; i++) {
+   for ( i = 0; i<n; i++) {
             arr[i] = sc.nextInt();
         }
         //solution
-        for(int i=0;i<10;i++){
+        boolean flag = false;
+        for( i=0;i<10;i++){
       if(arr[i]==x){
-        System.out.println("element found at "+i);
+        flag=true;
+        break;
       }
-      
-      
+        }
+        if(flag==true){
+                    System.out.println("element found at "+i);
+        }
+        else{
+              System.out.println("element not found" );
         }
      sc.close() ;
     }
