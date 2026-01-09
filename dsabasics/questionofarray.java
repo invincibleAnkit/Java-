e7f1fe201ad3a15c9a106ccc87b76592c5d71451
invincibleAnkit <rkrockcena461@gmail.com>
2026-01-09 18:18:19 +0000

    commit

--- dsabasics/questionofarray.java
+++ dsabasics/questionofarray.java
@@ -1,15 +1,19 @@
-
 import java.util.Scanner;
-public class outputofarray {
+public class questionofarray{
     public static void main(String[]  args){
          Scanner sc = new Scanner(System.in);
     int[] arr = new int[10];
-   for (int i = 0; i<10; i++) {
+    int n =arr.length;
+   for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
         //output for array
-        for(int i=0;i<10;i++){
-        System.out.print(arr[i]+" ");
+        for(int i=0;i < n;i++){
+            if(arr[i]<35){
+                System.out.print(arr[i]+" ");
+
+            }
+        
         }
      sc.close() ;
     }
