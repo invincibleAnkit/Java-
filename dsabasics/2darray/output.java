6e73fbef2711fc4e4a29ec40b6ea0d6159e78b78
invincibleAnkit <rkrockcena461@gmail.com>
2026-02-01 18:31:51 +0000

    com

---
+++ dsabasics/2darray/output.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class output{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int[][] arr = new int[20][30];
+        int n = sc.nextInt();
+         for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+             arr[i][j]=sc.nextInt();     
+            }
+       }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+           System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+       }
+    }
+}
