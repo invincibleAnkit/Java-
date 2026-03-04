a28db0fca82b5d37c1d077d2c59fb3b0629e9ece
invincibleAnkit <rkrockcena461@gmail.com>
2026-03-04 20:00:14 +0000

    commit3

---
+++ strings'/indexof.java
@@ -0,0 +1,13 @@
+public class indexof {
+    public static void main(String[] args){
+      String s = "Ankit Thakur";
+      System.out.println(s.indexOf('k'));
+      System.out.println(s.indexOf('z'));
+      System.out.println(s.lastIndexOf('k'));
+     
+      String a="abc";
+     String b="def";
+     System.out.println(a.compareTo(b));
+
+    }
+}
