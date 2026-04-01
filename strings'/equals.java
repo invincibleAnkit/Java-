8c6449b9a6605e477842c9a6521cc92603153d99
invincibleAnkit <rkrockcena461@gmail.com>
2026-04-01 18:49:39 +0000

    commit 1

---
+++ strings'/equals.java
@@ -0,0 +1,8 @@
+import java.util.Scanner;
+public class equals{
+    public static void main(String[] args){
+       String s ="ankit";
+       String t = "ankit";
+       System.out.println(s.equals(t)); 
+    }
+}
---
+++ strings'/uilder.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class uilder {
+    public static void main(String[] args){
+        StringBuilder sb =new StringBuilder("Ankit");
+       System.out.println(sb);
+       sb.setCharAt(1,'t');
+
+    }
+}
